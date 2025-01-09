6eae857b1b770921d8f59ec6177023be88e2173f
Hueyan Chen <hueyanchen2022@gmail.com>
2025-01-09 18:30:18 +0800

    更新app.py的訊息文字

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Hello World!"
+    return "Hello from the new feature branch!"
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
